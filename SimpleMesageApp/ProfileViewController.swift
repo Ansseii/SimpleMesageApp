3c7243d7c0bb9d7bf47216ce3bc65cfdd128b797
Aleksandr Lukash <a.lukash.92@gmail.com>
2020-09-13 20:16:57 +0300

    outlets have been added

--- SimpleMesageApp/ProfileViewController.swift
+++ SimpleMesageApp/ProfileViewController.swift
@@ -9,26 +9,18 @@
 import UIKit
 
 
-// For profile View
 class ProfileViewController: UIViewController {
   
+  @IBOutlet weak var profileImage: UIImageView!
+  
+  @IBOutlet weak var profileFullName: UILabel!
+  @IBOutlet weak var profileAbout: UILabel!
+  
   var currentProfile: Profile!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
+  }
 }
